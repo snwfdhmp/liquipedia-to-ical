e3493f9f11e83769c7ea50b7b36b3bc3e947f241
snwfdhmp <snwfdhmp@gmail.com>
2025-10-18 14:21:30 +0200

    add apiOptions to prepare for automations

--- meta/apiOptions.ts
+++ meta/apiOptions.ts
@@ -1,30 +1,68 @@
-export const apiOptions = {
-  competition_regex: {
-    allowedValues: "string",
+export interface ApiOption {
+  kind: "string" | "boolean" | "number"
+  title: string
+  urlParam: string
+  shouldShowInUi?: (o: ParserOptions) => boolean
+}
+
+export const apiOptions: Record<
+  Exclude<keyof ParserOptions, "url">,
+  ApiOption
+> = {
+  competitionRegex: {
+    kind: "string",
+    urlParam: "competition_regex",
+    title: "Filter based on competition",
   },
-  teams_regex: {
-    allowedValues: "string",
+  teamsRegex: {
+    kind: "string",
+    urlParam: "teams_regex",
+    title: "Filter based on teams",
   },
-  teams_regex_use_fullnames: {
-    allowedValues: ["true", "false"],
+  teamsRegexUseFullnames: {
+    kind: "boolean",
+    urlParam: "teams_regex_use_fullnames",
+    title: "Use team complete names for filtering",
+    shouldShowInUi: (o) => o.teamsRegex?.length > 0,
   },
-  condition_is_or: {
-    allowedValues: ["true", "false"],
+  conditionIsOr: {
+    kind: "boolean",
+    urlParam: "condition_is_or",
+    title: "Either team OR competition must match the filter",
+    shouldShowInUi: (o) =>
+      o.teamsRegex?.length > 0 && o.competitionRegex?.length > 0,
   },
-  ignore_tbd: {
-    allowedValues: "string",
+  ignoreTbd: {
+    kind: "boolean",
+    urlParam: "ignore_tbd",
+    title: 'Ignore events if both team names are "???"',
   },
-  verbose: {
-    allowedValues: "string",
-    hideInUI: true,
+  shouldVerbose: {
+    kind: "boolean",
+    urlParam: "verbose",
+    title: "Verbose mode",
+    shouldShowInUi: () => false,
   },
-  match_both_teams: {
-    allowedValues: ["true", "false"],
+  matchBothTeams: {
+    kind: "boolean",
+    urlParam: "match_both_teams",
+    title: "Require team A and team B to match the team filter",
+    shouldShowInUi: (o) => o.teamsRegex?.length > 0,
   },
-  past_match_allow_seconds: {
-    allowedValues: "number",
+  pastMatchAllowSeconds: {
+    kind: "number",
+    urlParam: "past_match_allow_seconds",
+    title: "Remove events older than X seconds",
   },
-  expect_missing_teams: {
-    allowedValues: ["true", "false"],
+  expectMissingTeams: {
+    kind: "boolean",
+    urlParam: "expect_missing_teams",
+    title: "Expect missing teams (useful for Battle Royale games)",
+    shouldShowInUi: () => false,
+  },
+  allowMissingTeams: {
+    kind: "boolean",
+    urlParam: "allow_missing_teams",
+    title: "Allow missing teams (useful for Battle Royale games)",
   },
 }
